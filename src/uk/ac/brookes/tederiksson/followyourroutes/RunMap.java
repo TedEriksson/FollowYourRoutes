f08ea442cafb1a929b052f933b94baf0b91d7f2f
Ted Eriksson <ted.eriksson@me.com>
2013-11-06 13:30:42 +0000

    got map working

---
+++ src/uk/ac/brookes/tederiksson/followyourroutes/RunMap.java
@@ -0,0 +1,15 @@
+package uk.ac.brookes.tederiksson.followyourroutes;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class RunMap extends FragmentActivity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_runmap);
+	}
+
+}
